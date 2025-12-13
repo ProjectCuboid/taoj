c4b1fae29574aac73803258b99ef299f7f239194
ProjectCuboid <itzcubical.code@gmail.com>
2025-12-13 15:19:24 +0400

    Committed from push.sh

--- uptime/src/worker.js
+++ uptime/src/worker.js
@@ -1,5 +1,10 @@
 export default {
     async fetch(req) {
-        return new Response('Hello World!', { status: 200 });
+        const url = 'https://raw.githubusercontent.com/ProjectCuboid/taoj/refs/heads/main/uptime/localhtml/index.html';
+        const upstream = await fetch(url);
+        const body = await upstream.text();
+        const headers = new Headers(upstream.headers);
+        headers.set('Content-Type', 'text/html; charset=utf-8');
+        return new Response(body, { status: upstream.status, headers });
     }
 };
